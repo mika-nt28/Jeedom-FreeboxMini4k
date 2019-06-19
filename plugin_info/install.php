eeb3ec772b4b44fdda6e284db40d23f9b26d6c38
Halbout Michael <michaelhalbout@gmail.com>
2019-06-19 14:28:47 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -1,10 +1,9 @@
 <?php
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 function FreeboxMini4k_install() {	
-	FreeboxMini4k::CreateArchi();
+	exec("sudo chmod 755 /var/www/html/plugins/FreeboxMini4k/ressources/mini4k_cmd");
 }
 function FreeboxMini4k_update() {
-	FreeboxMini4k::CreateArchi(); 
 	exec("sudo chmod 755 /var/www/html/plugins/FreeboxMini4k/ressources/mini4k_cmd");
 }
 function FreeboxMini4k_remove() {
